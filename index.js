77bbd4c8767da8df396635c90a79bf8ee3999177
alexandrosmrv <95053670+alexandrosmrv@users.noreply.github.com>
2023-09-19 21:07:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+$(".form-check").hover(function() {
+    $(this).addClass("containerClassJs");
+ }, function(){
+    $(this).removeClass("containerClassJs"); 
+ });
+ $(".savebutton").hover(function() {
+    $(this).addClass("savebuttonjs");
+ }, function(){
+    $(this).removeClass("savebuttonjs"); 
+ });
+ $(".imgcls").hover(function() {
+   $(this).addClass("imgjs", 100000);
+}, function(){
+   $(this).removeClass("imgjs", 100000); 
+});
