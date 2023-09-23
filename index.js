f7525530aecde6ed47ce1186f2ab5812862d391b
alexandrosmrv <95053670+alexandrosmrv@users.noreply.github.com>
2023-09-23 17:58:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,48 @@ $(".form-check").hover(function() {
 }, function(){
    $(this).removeClass("imgjs", 100000); 
 });
+
+let datePlace = document.getElementById("realDate") 
+datePlace.innerText = theDay()+" "+ theNumDay()+" "+ theMonth() + " " + theYear()
+
+function theDay (){
+   var d= new Date();
+   var theday= d.getDay();
+ if (theday === 1 ){
+   return ("Monday");
+ }
+ else if (theday===2){
+   return ("Tuesday")
+ }
+else if (theday===3){
+   return ("Wednesday")
+}
+else if(theday===4){
+   return("Thursday")
+}
+else if (theday===5){
+   return ("Friday")
+}
+else if (theday===6){
+   return("Saturday")
+}
+else{return ("Sunday")}
+}
+function theNumDay (){
+   var tnm = new Date();
+   var thenumday= tnm.getDate();
+   return thenumday;
+
+}
+function theMonth (){
+   var m = new Date();
+   var themonth= (m.getMonth())+1;
+   return themonth;
+}
+
+function theYear (){
+   var y = new Date();
+   var theyear = y.getFullYear();
+   return theyear;
+
+}
